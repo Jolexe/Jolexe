8645f5023422a1905cd8ecf6febe566696c70213
Joris_Pienne <piennejo@gmail.com>
2019-09-20 09:53:03 +0200

    Modif ajout header/footer

--- header.php
+++ header.php
@@ -1,4 +1,12 @@
-<header>
-  <h1>	Salut les pelos </h1>
-  <img src="Screenshot.png" alt="yusk2">
-</header>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <header>
+      <h1>	Salut les pelos </h1>
+      <img src="Screenshot.png" alt="yusk2">
+    </header>
