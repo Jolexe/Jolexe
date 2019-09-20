3c96834fffbd86a6b00d01d54f7e34a404605664
Joris_Pienne <piennejo@gmail.com>
2019-09-20 11:15:08 +0200

    Import css + ajout css header

--- header.php
+++ header.php
@@ -3,10 +3,15 @@
   <head>
     <meta charset="utf-8">
     <meta name="author" content="Alexandra & Joris">
-    <title> Jolexe </title>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/reset.css A MODIF /!\">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/index.css">
+    <title> <?php bloginfo('name');?> - <?php bloginfo('description');?> </title>
   </head>
   <body>
 
     <header>
-      <h1> Salut les pelos, c'est le header </h1>
+      <h1>  <?php bloginfo('name');?> </h1>
+
+      <h2>  <?php bloginfo('description');?> </h2>
+
     </header>
